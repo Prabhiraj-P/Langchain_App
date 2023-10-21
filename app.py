6d1ee41d160b592fd4e5edf63d41811c2e346137
Deadcode87 <128582548+Deadcode87@users.noreply.github.com>
2023-10-21 21:17:31 +0530

    solved altair verson error

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
+from altair.vegalite.v4.api import Chart
 import streamlit as st 
-from altair.vegalite.v4 import Chart
 from streamlit_extras.add_vertical_space import add_vertical_space
 
 with st.sidebar:
+
     st.title("LLM Chat")
     st.markdown('''
         #About
         This application is an LLM-based chatbot built using:
          - [Streamlit](https://streamlit.io/)
     - [LangChain](https://python.langchain.com/)
-    - [OpenAI](https://platform.openai.com/docs/models) LLM model
+    - [OpenAI](https://platform.openai.com/docs/models) LLM model------
  
     '''
     )
+   
