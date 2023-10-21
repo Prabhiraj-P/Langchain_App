8b86bb3b3de1b42238e47eb3876fcbd826955d81
Deadcode87 <128582548+Deadcode87@users.noreply.github.com>
2023-10-21 22:36:42 +0530

    Web framework created

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from altair.vegalite.v4.api import Chart
 import streamlit as st 
+from PyPDF2 import PdfReader
 from streamlit_extras.add_vertical_space import add_vertical_space
 
 with st.sidebar:
@@ -9,9 +10,35 @@ with st.sidebar:
         #About
         This application is an LLM-based chatbot built using:
          - [Streamlit](https://streamlit.io/)
-    - [LangChain](https://python.langchain.com/)
-    - [OpenAI](https://platform.openai.com/docs/models) LLM model------
+         - [LangChain](https://python.langchain.com/)
+         - [OpenAI](https://platform.openai.com/docs/models) 
  
     '''
     )
+
+
+def main():
+    
+    st.write("hello")
+
+    # Upload  PDF
+    pdf=st.file_uploader("Upload your pdf",type='pdf')
+    if pdf is not None:
+
+     pdf_reader=PdfReader(pdf)
+
+     text=""
+
+     for pages in pdf_reader.pages:
+
+        text +=pages.extract_text()
+     st.write(text)
+
+
+
+
+
+if __name__=='__main__':
+    
+     main()
    
