89f93b870d44ae8d3223270776d178afc95596fc
Deadcode87 <128582548+Deadcode87@users.noreply.github.com>
2023-10-22 15:48:38 +0530

    |>

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from altair.vegalite.v4.api import Chart
 import streamlit as st 
+from dotenv import load_dotenv
+import pickle
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from PyPDF2 import PdfReader
+from langchain.vectorstores import FAISS
 from streamlit_extras.add_vertical_space import add_vertical_space
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.callbacks import get_openai_callback
+import os
 
 with st.sidebar:
 
@@ -18,7 +25,8 @@ with st.sidebar:
 
 
 def main():
-    
+
+    load_dotenv()
     st.write("hello")
 
     # Upload  PDF
@@ -26,6 +34,9 @@ def main():
     if pdf is not None:
 
      pdf_reader=PdfReader(pdf)
+     st.write(pdf.name)
+
+     store_name=pdf.name[:-4]
 
      text=""
 
@@ -33,7 +44,39 @@ def main():
 
         text +=pages.extract_text()
      st.write(text)
+     text_splitter=RecursiveCharacterTextSplitter(
+            chunk_size=1000,
+            chunk_overlap=200,
+            length_function=len
+            )
+     chunks=text_splitter.split_text(text=text)
+    
+    
+     #emmbedding
+     if os.path.exists(f"{store_name}.pkl"):
+        with open(f"{store_name}.pkl", "rb") as f:
+            VectorStore = pickle.load(f)
+            # st.write('Embeddings Loaded from the Disk')s
+     else:
+        embeddings = OpenAIEmbeddings()
+        VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
+        with open(f"{store_name}.pkl", "wb") as f:
+            pickle.dump(VectorStore, f)
+    
 
+     #Accept user question
+     query = st.text_input("Ask questions about your PDF file:")
+        # st.write(query)
+ 
+    if query:
+            docs = VectorStore.similarity_search(query=query, k=3)
+ 
+            llm = OpenAI()
+            chain = load_qa_chain(llm=llm, chain_type="stuff")
+            with get_openai_callback() as cb:
+                response = chain.run(input_documents=docs, question=query)
+                print(cb)
+            st.write(response)
 
 
 
